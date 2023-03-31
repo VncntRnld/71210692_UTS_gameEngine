36925a6a60559a07ac05bd69b416b5bca014cf39
VncntRnld <vincent.ronald@ti.ukdw.ac.id>
2023-03-31 17:30:37 +0700

    Upload Jawaban ke-2

---
+++ Assets/Script/menuUtama.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuUtama : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
